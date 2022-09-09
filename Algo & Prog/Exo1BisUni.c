393d9a524eb9e25634cfe5a3a2921d3fb2384694
VaroWz <antoine.bouille.24@gmail.com>
2022-09-09 19:06:25 +0200

    Create Exo1BisUni.c

---
+++ Algo & Prog/Exo1BisUni.c
@@ -0,0 +1,16 @@
+//
+// Created by antoi on 09/09/2022.
+//
+#include <stdio.h>
+int main(){
+
+    printf("Entrez le rayon de votre cercle\n");
+
+    int rayon;
+    int pi = 4.14;
+
+    scanf("%i", &rayon);
+
+    printf("Le diamètre de votre cercle vaut: %i", rayon*pi);
+
+}
